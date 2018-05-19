e5fe97329c6a4e61f08e5c2dcff1be5ae8f31914
Amos <asidelnik@gmail.com>
2018-05-19 10:07:27 +0300

    Working get weather api

--- public/main.js
+++ public/main.js
@@ -1,5 +1,31 @@
 // Importing classes modules Events, Data, & Render
-import Events from './events.js';
-import Data from './data.js';
-import Render from './render.js';
+// import Events from './events.js';
+// import Data from './data.js';
+// import Render from './render.js';
 
+
+$(document).ready(function () {
+   var city = "Bangalore";
+   var searchtext =
+      "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" +
+      city + "') and u='c'"
+   //change city variable dynamically as required
+   $.get({
+      url: "https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json", // just a demo - place here your api details
+      success: function (data) {
+         console.log(data);
+         $('#temp').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp +
+            "°C");
+      }
+   });
+});
+
+
+
+
+// $.getJSON("https://query.yahooapis.com/v1/public/yql?q=" + searchtext + "&format=json").success(function (
+//       data) {
+//       console.log(data);
+//       $('#temp').html("Temperature in " + city + " is " + data.query.results.channel.item.condition.temp +
+//          "°C");
+//    });
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 var express = require('express');
+var path = require('path');
+
+//  app.listen(8080);
+const SERVER_PORT = 8080;
+
 var app = express();
 app.use(express.static('public'));
-
-app.get('/', function(request, response){
-   console.log("Hello World");
- });
+app.use(express.static('files'));
+app.use(express.static('node_modules'));
 
 
+// app.get('/', function (request, response) {
+//    response.send("Hey, hello from the server!");
+// });
+
+app.get('/', function (request, response) {
+   //response.send("Hey, hello from the server!");
+   response.sendFile(path.join(public + '/me.html'));
+});
 
 
- app.listen(8080);
+
+app.listen(SERVER_PORT, () => {
+   console.log("Server started on port " + SERVER_PORT);
+});
