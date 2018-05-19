89c759ad94c1dad8f1a2ed9c3cb489b0e76ea544
Amos <asidelnik@gmail.com>
2018-05-19 17:38:38 +0300

    Events.fetchCity returns api's ajax promise of searched city temp. No data or render yet.

--- public/events.js
+++ public/events.js
@@ -3,16 +3,23 @@ class Events {
 
    }
 
+   // Evnent listener - gets city temp on search
    fetchCity(weatherAPI) {
       $('#getTemp').on('click', function () {
          let city = $('#city').val();
+         // Executes api's getTemp method
          weatherAPI.getTemp(city)
-            .then(function (data) {
-               console.log(data);
+            .then(function (data) {   // When promise is returned, then these commands are preformed
+               let temp = data.query.results.channel.item.condition.temp;
+               $('#temp').html("Temperature in " + city + " is " + temp + "°C");
+            })
+            .catch(function (error) { //If the ajax promise returns an error, the catch will notify it
+               console.log(error.data)
             })
       })
    }
 }
+
 
 export default Events;
 
@@ -23,8 +30,9 @@ export default Events;
    4. Gets a promise
    5. Logs that promise
 
-
    $('#temp').html("Temperature in " + city + " is " + 
-   data.query.results.channel.item.condition.temp + "°C");
+               data.query.results.channel.item.condition.temp + "°C");
+
+   
 
 */
